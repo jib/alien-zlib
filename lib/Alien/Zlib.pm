f56bc0e6698ecefadb515b4843c3e90c50fe71b0
Jos Boumans <kane@cpan.org>
2006-08-17 20:50:30 +0000

    r2509@codebase2:  kane | 2006-08-17 22:19:41 +0200  Commit history from perforce during svk import:
     Change 11656 on 2004/12/16 by coral@cay
      
      	* clean up extra end-of-line whitespace
      
      Change 11560 on 2004/12/07 by coral@cay
      
      	* spelling fix: "it's" -> "its"
      
      Change 10641 on 2004/05/22 by kane@myriad
      
      	* This marks Alien::Zlib 0.00_01
        * update add_libs to include File::Fetch
        * update manifest
        * update meta.yaml (automatically)
        * finish makefile.pl to patch zlib sources
        * update Zlib.pm docs
        * add version method
        * add test for version method
      
      Change 8488 on 2003/10/19 by kane@myriad
      
      	* Add alien::zlib	* Add alien::zlib	* Add alien::zlib	* Add alien::zlib	* Add alien::zlib	* Add alien::zlib	* Add alien::zlib	* Add alien::zlib	
      
     
    
    
    git-svn-id: http://oss.dwim.org/alien-zlib@392 4dccba1d-3c1b-0410-aec4-feb514a8dabc

---
+++ lib/Alien/Zlib.pm
@@ -0,0 +1,166 @@
+package Alien::Zlib;
+
+use strict;
+use vars qw[$VERSION];
+
+use File::Spec;
+
+$VERSION = '0.00_01';
+
+### sanity check
+### only do this one 'use' time, because we use Alien::Zlib from
+### the Makefile.PL as well...
+sub import {
+    require Carp;
+    for my $file ( __PACKAGE__->include_files, __PACKAGE__->lib_files ) {
+        Carp::croak "Missing file: '$file'\n" unless -e $file;
+    }
+}
+
+=head1 NAME
+
+Alien::Zlib
+
+=head1 SYNOPSIS
+
+    require Alien::Zlib;
+
+    my $prefix  = Alien::Zlib->prefix;
+    my $include = Alien::Zlib->include;
+    my $lib     = Alien::Zlib->lib;
+
+    my $ver     = Alien::Zlib->version;
+
+=head1 DESCRIPTION
+
+This module resolves the non-perl prerequisite C<zlib>.
+This allows your program/module to depend on the C<zlib> library and
+expect common installers, such as C<CPANPLUS> and C<CPAN.pm> to
+install them for you.
+
+It does so by installing a private copy of the zlib libraries into
+its module directory.
+
+Go to L<http://www.zlib.org> for more information about C<zlib>.
+
+=head1 METHODS
+
+=head2 prefix();
+
+Returns the prefix (ie, the directory) where the C<include> and C<lib>
+directores will live.
+On a standard unix system, this might be C</usr/local/>
+
+For this module, it will be C<$INC{'Alien/Zlib'}>.
+
+=cut
+
+my $prefix;
+sub prefix {
+    return $prefix if $prefix;
+
+    my $ext = '.pm';
+    $prefix = join '/', split '::', __PACKAGE__;
+    $prefix = File::Spec->rel2abs( $INC{$prefix.$ext} );
+    $prefix =~ s/$ext$//;
+
+    return $prefix;
+}
+
+=head2 lib();
+
+Returns the full path to the C<lib> directory for the C<zlib>
+libraries.
+On standard unix systems, this might be C</usr/local/lib>
+
+For this module, it will be C<$INC{'Alien/Zlib'}/lib>
+
+=head2 include()
+
+Returns the full path to the C<include> directory for the C<zlib>
+libraries.
+On standard unix systems, this might be C</usr/local/include>
+
+For this module, it will be C<$INC{'Alien/Zlib'}/include>
+
+=cut
+
+for my $sub (qw[lib include]) {
+    no strict 'refs';
+    *$sub = sub {
+                my $self = shift;
+                return File::Spec->catdir( $self->prefix, $sub );
+            };
+}
+
+=head2 version()
+
+Returns the version of the zlib libraries installed by this version
+of C<Alien::Zlib>
+
+=cut
+
+sub version { return '1.2.1'; }
+
+=head2 lib_files()
+
+Returns a list of full paths to the files that can be found in the
+'Alien::Zlib->lib' directory. This corresponds to all created
+libraries.
+
+=head2 include_files()
+
+Returns a list of full paths to the files that can be found in the
+'Alien::Zlib->include' direcotry. This corresponds to all copied
+header files.
+
+=cut
+
+my $files = {
+    include => [qw|zconf.h zlib.h|],
+    lib     => [qw|libz.a|],
+};
+
+for my $acc (qw[include_files lib_files]) {
+    no strict 'refs';
+    *$acc = sub {
+                my $self    = shift;
+                my ($type)  = $acc =~ /(\w+)_files/;
+
+                return map {
+                            File::Spec->catfile( $self->$type(), $_)
+                        } @{$files->{$type}};
+    }
+}
+
+=head1 TODO
+
+=over 4
+
+=item *
+
+Probe for system installed version of C<zlib>
+
+=item *
+
+Gather more C<hints> file to ensure this module builds properly on
+all platforms that C<zlib> builds on.
+
+=head1 AUTHOR
+
+This module by
+Jos Boumans E<lt>kane@cpan.orgE<gt>.
+
+=head1 COPYRIGHT
+
+This module is
+copyright (c) 2003 Jos Boumans E<lt>kane@cpan.orgE<gt>.
+All rights reserved.
+
+This library is free software;
+you may redistribute and/or modify it under the same
+terms as Perl itself.
+
+=cut
+
+1;
